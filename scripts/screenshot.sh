c69adc05056af9ccff2172a84cae52132288d952
fecet <xiezej@gmail.com>
2025-08-24 11:44:50 +0800

    feat: copy screenshot

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,2 +1,6 @@
 # grim -g "$(slurp -b '#2E2A1E55' -c '#fb751bff')" -t ppm - | satty -f -
-grimblast --freeze --notify save area - | satty -f -
+grimblast --freeze --notify save area - |
+  satty -f - \
+    --copy-command=wl-copy \
+    --actions-on-enter=save-to-clipboard \
+    --early-exit
